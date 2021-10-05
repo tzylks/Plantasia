b16336fc668c93d566a7f8e08077595a2e03ffc5
TrevorZylks <trevorzylks@gmail.com>
2021-10-05 16:22:03 -0700

    claire

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -4,6 +4,6 @@ class FallbackController < ActionController::Base
 
   def index
     # React app index page
-    render file: 'public/index.html'
+    render file: '../client/public/index.html'
   end
 end
